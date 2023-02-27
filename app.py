5ff1805aace32caf781dd7671c0cf9f91c6e1651
Prashant Dhakad <prashantdhakad8001@gmail.com>
2023-02-27 23:02:00 +0530

    Building Blog-Website

--- app.py
+++ app.py
@@ -2,15 +2,62 @@ from flask import  Flask, render_template, flash
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from flask_sqlalchemy import  SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+#Add Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+#Secret Key!
 app.config['SECRET_KEY'] = "Secret Key"
+#Initialize The Database
+db = SQLAlchemy(app)
+
+#Create Models
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable = False)
+    email = db.Column(db.String(120), nullable = False, unique = True)
+    date_added = db.Column(db.DateTime, default = datetime.utcnow)
+    
+    def __repr__(self):
+        return '<Name %rr>' % self.name
+    
+#Ceate a user form
+#Ceate a form class
+class UserForm(FlaskForm):
+    name = StringField("Name", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired()])
+    
+    submit = SubmitField("Submit")
+
 
 #Ceate a form class
 class NamerForm(FlaskForm):
     name = StringField("What's is your name?", validators=[DataRequired()])
     submit = SubmitField("Submit")
 
+
+@app.route('/user/add',  methods = ['GET', 'POST'])
+def add_user():
+    name = None
+    form = UserForm()
+    #Validae Form
+    if form.validate_on_submit():
+        user = Users.query.filter_by(email = form.email.data).first()
+        if user is None:
+            user  = Users(name = form.name.data, email = form.email.data)
+            db.session.add(user)
+            db.session.commit()
+        name = form.name.data
+        form.name.data = ""
+        form.email.data = ""
+        our_users = Users.query.order_by(Users.id)
+        flash("User added successfully!")
+    return render_template("add_user.html",
+                           form = form,
+                           name = name,
+                           our_users = our_users)
 
 @app.route('/')
 def index():
@@ -54,6 +101,7 @@ def name():
                            form = form
                            )
     
+
 
 
 if __name__ == "__main__":
